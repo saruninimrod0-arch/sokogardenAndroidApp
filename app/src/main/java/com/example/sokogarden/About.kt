5b5db38d9d6dc38198bf694fce8817a062bf0056
saruninimrod0-arch <saruninimrod0@gmail.com>
2026-04-17 14:04:35 +0300

    add

--- app/src/main/java/com/example/sokogarden/About.kt
+++ app/src/main/java/com/example/sokogarden/About.kt
@@ -1,12 +1,21 @@
 package com.example.sokogarden
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class About : AppCompatActivity() {
+
+    // Declare A variable that will hold the text to speech object
+    lateinit var tts : TextToSpeech
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,28 @@ class About : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // find the textview and the button by use of their ids
+        val textView = findViewById<TextView>(R.id.aboutTxt)
+        val speakButton = findViewById<Button>(R.id.btnListen)
+
+        tts = TextToSpeech(this){
+            if(it == TextToSpeech.SUCCESS){
+                tts.language = Locale.US
+            }
+        }//end
+        speakButton.setOnClickListener {
+            val text = textView.text.toString()
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+
     }
+
+    override fun onDestroy() {
+        tts.stop()
+        tts.shutdown()
+        super.onDestroy()
+    }
+
+
+
 }
